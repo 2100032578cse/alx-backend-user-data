78b13c14d13c750efd9f0eee138e848c6f47ffad
Sandrine <99196826+2100032578cse@users.noreply.github.com>
2024-08-02 07:50:55 +0530

    Update encrypt_password.py

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -1,1 +1,19 @@
-i
+#!/usr/bin/env python3
+"""
+encrypting passwords module
+"""
+import bcrypt
+
+
+def hash_password(password: str) -> bytes:
+    """
+    Hashes a password using a random salt.
+    """
+    return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """
+    The Checks is the given password was formed to a hashed password.
+    """
+    return bcrypt.checkpw(password.encode('utf-8'), hashed_password)
